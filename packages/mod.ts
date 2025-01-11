bd71a63d28a773201bdb5ae5d5640d26349c560a
Imran Shitta-Bey <pickleboyonline@gmail.com>
2025-01-10 20:29:27 -0600

    notes

--- packages/mod.ts
+++ packages/mod.ts
@@ -2,5 +2,7 @@
  * Another question to ask is how to deal with external packages.
  * 
  * Most likely will just be a deno package, you import it like everything else, and from there
- * all the native code is linked properly
+ * all the native code is linked properly.
+ * 
+ * The dependencies will just have to all be merged and properly linked.
  */
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+// Goal is to make a basic counter app. Ideally, I will just put function
+// stubs and keep filling out the implementation to the point where I get what I want.
+// Do not focus too much on architecture/design as of now, just usage and implementation
